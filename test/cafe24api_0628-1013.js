9e9b798ec1c3432c1ae310b3db649fa984393180
webkers <57704835+webkers@users.noreply.github.com>
2023-06-28 10:32:20 +0900

    Update cafe24api_0628-1013.js

--- test/cafe24api_0628-1013.js
+++ test/cafe24api_0628-1013.js
@@ -1,20 +1,14 @@
 (async function (CAFE24API) {
+
 	var script = document.createElement('script');
 	script.type = 'module';
 	script.onload = function () {
 			console.log('podiz script start! // podiz.co.kr')
 	};
-	script.src = 'https://cdn.jsdelivr.net/gh/webkers/podiz-cdn@main/test/0628-1013/index.js';
+	script.src = 'https://cdn.jsdelivr.net/gh/webkers/podiz-cdn@main/test/0627-1834/index.js';
 	
 	document.head.appendChild(script); //or something of the likes
 
-	const getCartList = async (err, res) => {
-		if (err) {
-			console.log({ err });
-		} else {
-				console.log({ res });
-		}
-	}
 })(CAFE24API.init({
 	 client_id : 'OKIgBulXBlogAG4WeKsEhG',  // 사용할 앱의 App Key를 설정해 주세요.
 	 version : '2023-03-01'   // 적용할 버전을 설정해 주세요.
