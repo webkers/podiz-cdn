24c6cb3ef5bc3a8b52be789069e345e00b4c4af8
webkers <57704835+webkers@users.noreply.github.com>
2023-06-28 09:49:29 +0900

    Add files via upload

---
+++ test/cafe24api.js
@@ -0,0 +1,7 @@
+(async function (CAFE24API) {
+	console.log({ CAFE24API });
+
+})(CAFE24API.init({
+	 client_id : clientId,  // 사용할 앱의 App Key를 설정해 주세요.
+	 version : 'OKIgBulXBlogAG4WeKsEhG'   // 적용할 버전을 설정해 주세요.
+}));
