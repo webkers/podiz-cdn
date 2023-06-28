8ccc7a01628a716b975f3411299914a1e82f7db3
webkers <57704835+webkers@users.noreply.github.com>
2023-06-28 09:56:20 +0900

    Update cafe24api_0628-0953.js

--- test/cafe24api_0628-0953.js
+++ test/cafe24api_0628-0953.js
@@ -1,6 +1,12 @@
 (async function (CAFE24API) {
 	console.log({ CAFE24API });
-
+	const getCartList = async (err, res) => {
+		if (err) {
+			console.log({ err });
+		} else {
+			console.log({ res });
+		}
+	}
 })(CAFE24API.init({
 	 client_id : 'OKIgBulXBlogAG4WeKsEhG',  // 사용할 앱의 App Key를 설정해 주세요.
 	 version : '2023-03-01'   // 적용할 버전을 설정해 주세요.
