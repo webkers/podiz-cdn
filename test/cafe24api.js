d586b91e002122ed4eaa4e3c709cb9ff5dacdabb
webkers <57704835+webkers@users.noreply.github.com>
2023-06-28 09:52:45 +0900

    Delete cafe24api.js

--- test/cafe24api.js
+++
@@ -1,7 +0,0 @@
-(async function (CAFE24API) {
-	console.log({ CAFE24API });
-
-})(CAFE24API.init({
-	 client_id : 'OKIgBulXBlogAG4WeKsEhG',  // 사용할 앱의 App Key를 설정해 주세요.
-	 version : '2023-03-01'   // 적용할 버전을 설정해 주세요.
-}));
