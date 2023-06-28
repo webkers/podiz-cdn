1cebc23fba8818a0894737451946603776057681
webkers <57704835+webkers@users.noreply.github.com>
2023-06-28 09:52:29 +0900

    Update cafe24api.js

--- test/cafe24api.js
+++ test/cafe24api.js
@@ -2,6 +2,6 @@
 	console.log({ CAFE24API });
 
 })(CAFE24API.init({
-	 client_id : clientId,  // 사용할 앱의 App Key를 설정해 주세요.
-	 version : 'OKIgBulXBlogAG4WeKsEhG'   // 적용할 버전을 설정해 주세요.
+	 client_id : 'OKIgBulXBlogAG4WeKsEhG',  // 사용할 앱의 App Key를 설정해 주세요.
+	 version : '2023-03-01'   // 적용할 버전을 설정해 주세요.
 }));
