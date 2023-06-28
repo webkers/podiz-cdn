3e912adb41e113f9301705335fc04f049403b194
webkers <57704835+webkers@users.noreply.github.com>
2023-06-28 10:03:32 +0900

    Update and rename cafe24api_0628-0956.js to cafe24api_0628-1003.js

--- test/cafe24api_0628-1003.js
+++ test/cafe24api_0628-1003.js
@@ -1,10 +1,18 @@
 (async function (CAFE24API) {
 	console.log({ CAFE24API });
+	var script = document.createElement('script');
+	script.onload = function () {
+			//do stuff with the script
+	};
+	script.src = 'https://cdn.jsdelivr.net/gh/webkers/podiz-cdn@main/test/0627-1834/index.js';
+	
+	document.head.appendChild(script); //or something of the likes
+
 	const getCartList = async (err, res) => {
 		if (err) {
 			console.log({ err });
 		} else {
-			console.log({ res });
+				console.log({ res });
 		}
 	}
 })(CAFE24API.init({
