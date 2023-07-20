4161a9c2c6d15b30a852c86043804d16df7c9948
webkers <57704835+webkers@users.noreply.github.com>
2023-07-20 14:47:30 +0900

    Update 3.00.js

--- cafe24/3.00.js
+++ cafe24/3.00.js
@@ -1,1 +1,15 @@
+(async function (CAFE24API) {
 
+	var script = document.createElement('script');
+	script.type = 'module';
+	script.onload = function () {
+			console.log('podiz script start! // podiz.co.kr')
+	};
+	script.src = 'https://cdn.jsdelivr.net/gh/webkers/podiz-cdn@main/3.00/index.js';
+	
+	document.head.appendChild(script); //or something of the likes
+
+})(CAFE24API.init({
+	 client_id : 'OKIgBulXBlogAG4WeKsEhG',  // 사용할 앱의 App Key를 설정해 주세요.
+	 version : '2023-03-01'   // 적용할 버전을 설정해 주세요.
+}));
